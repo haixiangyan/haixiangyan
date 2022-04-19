a9b7fcb13954719dc89a26a8bd46d225f5306607
haixiangyan <Qq680321>
2022-04-19 12:50:27 +0800

    feat: 更新获取天气的 API

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const ejs = require('ejs');
 const path = require('path');
 const prettify = require('html-prettify');
 
-const getWeather = require('./apis/weather')
+const { getWeather, getIconUrl, getStateCNName } = require('./apis/weather')
 const data = require('./constants/data');
 const getBackground = require("./apis/background");
 
@@ -23,6 +23,8 @@ const main = async () => {
   const html = ejs.render(tplStr, {
     ...data,
     weather,
+    getIconUrl,
+    getStateCNName,
     background
   });
 
